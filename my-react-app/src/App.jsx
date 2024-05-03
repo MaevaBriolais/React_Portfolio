d56d6fd53094f10b37ecbe1a727815bb151ed958
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-03 16:00:08 +0200

    Ajout des NavBar et Pages

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import NavbarInterne from "./components/NavbarInterne";
+import NavbarPages from "./components/NavbarPages";
 
 function App() {
   return (
     <>
+      <NavbarPages/>
       <Header />
-      <p> PAGE PRINCIPALE </p>
+      <NavbarInterne/>
     </>
   );
 }
---
+++ my-react-app/src/components/NavbarPages.jsx
@@ -0,0 +1,15 @@
+import "../CSS/navbar.scss";
+
+function NavbarPages() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="">Diplômes</a></li>
+        <li><a href="">Expériences</a></li>
+        <li><a href=""> Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavbarPages;
