ddc54fd06c1219149f2d17d4c041639f3f481d9b
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-03 17:04:03 +0200

    Préparation du main pour la page Contact

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import App from './App.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    path: "/",
+    
+     children: [
+      {
+        path: "/ContactPage",
+        element: <ContactPage/>
+      }
+     ]
+},
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
