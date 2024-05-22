1652f93ec8991069cf5e6761aed7db1d1f68803b
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-22 08:08:52 +0400

    Installation SwiperSlide and new version of the carousel

--- my-react-app/src/components/Skills.jsx
+++ my-react-app/src/components/Skills.jsx
@@ -1,18 +1,38 @@
-import { Carousel } from "react-responsive-carousel";
 import skillsList from "../data/skillsData";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../CSS/skills.scss"
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Keyboard, Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 function Skills() {
   return (
-    <Carousel>
+    <Swiper  
+    slidesPerView={2}
+    spaceBetween={0}
+    keyboard={{
+      enabled: true,
+    }}
+    pagination={{
+      clickable: true,
+    }}
+    navigation={true}
+    modules={[Keyboard, Pagination, Navigation]}
+    >
       {skillsList.map((slide) => (
-        <div className="skills" key={slide.id}>
+      <SwiperSlide key={slide.id}>
+        <div className="skills" >
             <img src={slide.imgSrc} alt= {slide.name} />
           <p>{slide.name}</p>
         </div>
+      </SwiperSlide>
       ))}
-    </Carousel>
+      
+    </Swiper>
   );
 }
 export default Skills;
