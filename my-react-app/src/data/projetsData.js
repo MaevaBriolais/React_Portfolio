fef5619d40819e99caf4f749edf8fa19c629d9ee
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-22 09:21:54 +0400

    Create file projetsData.js

---
+++ my-react-app/src/data/projetsData.js
@@ -0,0 +1,39 @@
+const projetsData = [
+  {
+    id: 1,
+    name: "Projet 1 - Lego Muscu",
+    date: "Mars 2024 - Wild Code School",
+    duree: "",
+    github:
+      "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-P1-Lego-Muscu",
+    image: "",
+    site: "https://wildcodeschool-2024-02.github.io/JS-RemoteFR-P1-Lego-Muscu/",
+    techno: "HTML, CSS, JS Vannilla, Git/Github",
+    divers: "Site responsive",
+  },
+  {
+    id: 2,
+    name: "Projet 2 - AventuresAtmo",
+    date: "Avril 2024 - Wild Code School",
+    duree: "",
+    github:
+      "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-Gridception-P2-Plapimad",
+    image: "",
+    site: "https://aventuresatmo.netlify.app/",
+    techno: "React, Javascript, Node.js, Sass, API, Git/Github, Méthode Agile SCRUM",
+    divers: "Site responsive",
+  },
+  {
+    id: 3,
+    name: "Protojam - CheminVert",
+    date: "Mai 2024 - Wild Code School",
+    duree: "36 heures",
+    github:
+      "",
+    image: "https://github.com/WildCodeSchool-2024-02/protojam-team-E",
+    site: "",
+    techno: "React, Javascript, Tailwind, Git/Github",
+    divers: "Site responsive",
+  },
+];
+export default projetsData;
